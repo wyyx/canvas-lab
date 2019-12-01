b6ea1cf0ade107fe93299e4805668d35c5e5f3fc
chencheng <chencheng@xydqq.com>
2019-12-01 23:13:22 +0800

    Displaying the map on the canvas

--- js/main.ts
+++ js/main.ts
@@ -10,19 +10,45 @@ function canvasApp() {
   var theCanvas = document.getElementById('canvas') as HTMLCanvasElement
   var context = theCanvas.getContext('2d')
 
-  drawScreen()
-
+  var tileSheet = new Image()
+  tileSheet.addEventListener('load', eventSheetLoaded, false)
+  tileSheet.src = '../images/tanks_sheet.png'
+  var mapIndexOffset = -1
+  var mapRows = 10
+  var mapCols = 10
+  var tileMap = [
+    [32, 31, 31, 31, 1, 31, 31, 31, 31, 32],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [32, 1, 26, 1, 26, 1, 26, 1, 1, 32],
+    [32, 26, 1, 1, 26, 1, 1, 26, 1, 32],
+    [32, 1, 1, 1, 26, 26, 1, 26, 1, 32],
+    [32, 1, 1, 26, 1, 1, 1, 26, 1, 32],
+    [32, 1, 1, 1, 1, 1, 1, 26, 1, 32],
+    [1, 1, 26, 1, 26, 1, 26, 1, 1, 1],
+    [32, 1, 1, 1, 1, 1, 1, 1, 1, 32],
+    [32, 31, 31, 31, 1, 31, 31, 31, 31, 32]
+  ]
+  function eventSheetLoaded() {
+    drawScreen()
+  }
   function drawScreen() {
-    context.strokeStyle = 'red'
-    context.lineWidth = 5
-    context.moveTo(0, 0)
-    context.lineTo(50, 0)
-    context.lineTo(50, 50)
-    context.stroke()
-    var isPoint1InPath1 = context.isPointInPath(0, 0)
-    var isPoint1InPath2 = context.isPointInPath(25, 100)
-    console.log('isPoint1InPath1=' + isPoint1InPath1)
-    console.log('isPoint1InPath2=' + isPoint1InPath2)
-    context.closePath()
+    for (var rowCtr = 0; rowCtr < mapRows; rowCtr++) {
+      for (var colCtr = 0; colCtr < mapCols; colCtr++) {
+        var tileId = tileMap[rowCtr][colCtr] + mapIndexOffset
+        var sourceX = Math.floor(tileId % 8) * 32
+        var sourceY = Math.floor(tileId / 8) * 32
+        context.drawImage(
+          tileSheet,
+          sourceX,
+          sourceY,
+          32,
+          32,
+          colCtr * 32,
+          rowCtr * 32,
+          32,
+          32
+        )
+      }
+    }
   }
 }
